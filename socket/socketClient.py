afb5d93d2a93857f72a4e7faa0802752a28cdcb8
1194893728 <1194893728@qq.com>
2020-03-17 19:16:51 +0800

    20200317修改

--- socket/socketClient.py
+++ socket/socketClient.py
@@ -1,22 +1,33 @@
+# coding=utf-8
 import socket
 import sys
 
 # 创建socket 对象
 socketClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # 获取本地主机名
 host = socket.gethostname()
-
 # 设置端口号
 port = 8088
+# 设置状态
+alive = True
 
-# 设置端口号
+# 根据端口号和ip连接服务器
 socketClient.connect((host, port))
 
-socketClient.send("测试".encode())
-socketClient.send("测试".encode())
-socketClient.send("测试".encode())
-socketClient.send("测试".encode())
+# 发送消息
+socketClient.send(("{\"type\":\"OPEN\",\"clientName\":\"" + str(port) + "\"}").encode("utf-8"))
+
+print("已登录")
+
+while alive:
+    print("正在输入："),
+    line = input()
+
+    # 判断是否退出
+    if (line == 'q') | (line == 'Q'):
+        break
+    else:
+        socketClient.send(("{\"type\":\"MESSAGE\",\"message\":\"" + line + "\",\"chat\":\"GROUP\"}").encode("utf-8"))
 
 # 接收小于 1024 字节的数据
 msg = socketClient.recv(1024)
